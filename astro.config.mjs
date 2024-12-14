60a104217fcbe8df4b6a91fc1751dcc5a3c5f49a
Moustapha Diaby <me@isaacdiaby.tech>
2024-12-14 23:23:27 +0000

    fix: Correct import statement for tailwind in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import { tailwind } from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
